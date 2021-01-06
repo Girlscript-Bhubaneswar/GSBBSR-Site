9f1c761526c0ed0b70f454b0e05b26c88b01ebc7
Adyasha Mohanty <adyashamohanty8105@gmail.com>
2021-01-06 19:02:33 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -6,7 +6,7 @@ let header = $(`
   <div id="myNav" class="fullScreen-navigation">
       <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
         <div class="navigation-content" id="smallnav">
-		  <a class="active" href="#hero">HOME</a>
+		  <a href="#hero">HOME</a>
 		  <a href="#aboutSection">ABOUT US</a>
           <a href="#eventsSection">ANNOUNCEMENTS</a>
           <a href="#team">OUR TEAM</a>
@@ -21,7 +21,7 @@ let header = $(`
 	<div class="collapse navbar-collapse justify-content-end" id="navbarNav">
 	  <ul class="navbar-nav ml-5">
 		<li class="nav-item">
-		  <a class="nav-link active"  href="#hero"><span>HOME</span></a>
+		  <a class="nav-link"  href="#hero"><span>HOME</span></a>
 		</li>
 		<li class="nav-item">
 		  <a class="nav-link" href="#aboutSection"><span>ABOUT US</span></a>
